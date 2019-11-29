4800502d0441b83374b7cae48cacba2a01a1324c
Spencer Bangerter <52753876+SpencerBangerter@users.noreply.github.com>
2019-11-29 13:14:31 -0700

    Added base files. Base character generation

---
+++ generator.js
@@ -0,0 +1,83 @@
+
+
+let lowercaseGen = function () {
+    let letter = String.fromCharCode(Math.floor(Math.random()  * 26) + 97)
+    console.log(letter)
+}
+
+let uppercaseGen = function () {
+    let letter = String.fromCharCode(Math.floor(Math.random()  * 26) + 65)
+    console.log(letter)
+}
+
+let numberGen = function () {
+    let num = String.fromCharCode(Math.floor(Math.random()  * 10) + 48)
+    console.log(num)
+}
+
+let symbolGen = function () {
+    let symbolArr = [
+        '~',
+        '`',
+        '!',
+        '@',
+        '#',
+        '$',
+        '%',
+        '°',
+        '^',
+        '&',
+        '*',
+        '(',
+        ')',
+        '-',
+        '_',
+        '+',
+        '=',
+        '{',
+        '}',
+        '[',
+        ']',
+        '|',
+        '/',
+        ':',
+        ';',
+        '"',
+        '<',
+        '>',
+        ',',
+        '.',
+        '?',        
+    ]
+    let symbol = symbolArr[Math.floor(Math.random()  * symbolArr.length)]
+    console.log(symbol)
+}
+
+uppercaseGen()
+uppercaseGen()
+uppercaseGen()
+uppercaseGen()
+uppercaseGen()
+uppercaseGen()
+uppercaseGen()
+numberGen()
+numberGen()
+numberGen()
+numberGen()
+numberGen()
+numberGen()
+numberGen()
+numberGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
+symbolGen()
