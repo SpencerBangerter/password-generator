db2f34c727634b693ab5fc5c2b51c2d615e236e4
Spencer Bangterer <bangerter.spencer@gmail.com>
2019-12-01 14:03:21 -0700

    Working on a pivot away from "fromCharCode". The way I envision doing the actual generation doesnt play super nice with that.

--- generator.js
+++ generator.js
@@ -1,3 +1,56 @@
+// Final Password El
+const passwordEl = document.getElementById("password")
+
+// Password Gen Element Grabbers
+const lengthEl = document.getElementById("passLength")
+const lowercaseEl = document.getElementById("lcLetterCheck")
+const uppercaseEl = document.getElementById("ucLetterCheck")
+const numbersEl = document.getElementById("numCheck")
+const symbolsEl = document.getElementById("symbolsCheck")
+
+// Button Elements
+const generateEl = document.getElementById("generate")
+const clipboardEl = document.getElementById("clipboard")
+
+console.log(clipboardEl)
+
+// Event Listener for Generation Click
+
+generateEl.addEventListener("click", function () {
+    //initialize viable characters
+    var passwordOptions = '';
+
+    //pull values from elements above
+    let length = parseInt(lengthEl.value);
+    let lowercase = lowercaseEl.checked;
+    let uppercase = uppercaseEl.checked;
+    let numbers = numbersEl.checked;
+    let symbols = symbolsEl.checked;
+    // strings for value options
+    let poLower = 'abcdefghijklmnopqrstuvwxyz';
+    let poUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let poNum = '0123456789';
+    let poSym = '~!@#$%°^&*()-_+={}[]|/:;"<>.?';
+
+    // Sets viable characters based upon checkboxes
+    if (lowercase) {
+        passwordOptions += poLower
+    }
+    if (uppercase) {
+        passwordOptions += poUpper
+    }
+    if (numbers) {
+        passwordOptions += poNum
+    }
+    if (symbols) {
+        passwordOptions += poSym
+    }
+
+    console.log(passwordOptions)
+
+
+})
+
 // Generator Functions for random characters
 // Lowercase, Uppercase, and Number gen uses unicode character charts.  
 let lowercaseGen = function () {
@@ -53,31 +106,3 @@ let symbolGen = function () {
     console.log(symbol)
 }
 
-uppercaseGen()
-uppercaseGen()
-uppercaseGen()
-uppercaseGen()
-uppercaseGen()
-uppercaseGen()
-uppercaseGen()
-numberGen()
-numberGen()
-numberGen()
-numberGen()
-numberGen()
-numberGen()
-numberGen()
-numberGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
-symbolGen()
